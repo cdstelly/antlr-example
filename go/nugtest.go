3b9f87e33caa7afdd922b050fce9eb359360a15d
cdstelly <cdstelly@gmail.com>
2017-05-13 12:01:36 -0400

    from stdin

--- go/nugtest.go
+++ go/nugtest.go
@@ -21,23 +21,38 @@ func (this *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
 }
 
 func main() {
-        //fi := bufio.NewReader(os.NewFile(0, "stdin"))
-	fi, _ := antlr.NewFileStream(os.Args[1])
+	//fi, _ := antlr.NewFileStream(os.Args[1])
+	//fi, _ := antlr.NewInputStream(os.NewFile(0, "stdin"))
 
-	lexer := parser.NewNuggetLexer(fi)
-	stream := antlr.NewCommonTokenStream(lexer,0)
-	p := parser.NewNuggetParser(stream)
-	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
-	p.BuildParseTrees = true	//necessary?
-	tree := p.Nugget()
-	antlr.ParseTreeWalkerDefault.Walk(NewTreeShapeListener(), tree)
+	stdin := bufio.NewReader(os.NewFile(0, "stdin"))
+	for {
+		fmt.Printf("nugget> ")
+		var statement string
+		var ok bool
+		if statement, ok = readline(stdin); ok {
+
+			fi := antlr.NewInputStream(statement)
+
+			lexer := parser.NewNuggetLexer(fi)
+			stream := antlr.NewCommonTokenStream(lexer,0)
+			p := parser.NewNuggetParser(stream)
+			p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+			p.BuildParseTrees = true	//necessary?
+			tree := p.Nugget()
+			antlr.ParseTreeWalkerDefault.Walk(NewTreeShapeListener(), tree)
+
+		} else {
+			break
+		}
+	}
+
 }
 
 func readline(fi *bufio.Reader) (string, bool) {
-        s, err := fi.ReadString('\n')
-        if err != nil {
-                return "", false
-        }
-        return s, true
+	s, err := fi.ReadString('\n')
+	if err != nil {
+		return "", false
+	}
+	return s, true
 }
 
